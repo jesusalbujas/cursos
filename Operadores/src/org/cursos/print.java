48c2ca11db1d2b58b1fa27bed151a6389ff6823b
jesusalbujas <jesusramirez35000@gmail.com>
2024-08-28 16:21:25 -0400

    Add print

---
+++ Operadores/src/org/cursos/print.java
@@ -0,0 +1,14 @@
+package org.cursos;
+/* El programa debe mostrar «2 más 3 es igual a 5» en la pantalla. */
+public class Solution {
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 2;
+        
+		System.out.print(b);
+        System.out.print(" más ");
+        System.out.print(a);        
+        System.out.print(" es igual a ");
+        System.out.print(a + b);
+	}
+}
