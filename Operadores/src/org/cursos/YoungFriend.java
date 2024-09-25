05aba5ae4803107b7e8c7f17043262a58eeb5bab
jesusalbujas <jesusramirez35000@gmail.com>
2024-09-25 08:27:15 -0400

    Add Young Friend task

---
+++ Operadores/src/org/cursos/YoungFriend.java
@@ -0,0 +1,22 @@
+package org.cursos;
+
+/*
+Actualmente estamos en el año 3126. Mi amigo nació hace 8 años.
+Escribe un programa que muestre el año de nacimiento de mi amigo.
+
+Requirements:
+El programa debe generar texto.
+El año mostrado debe tener cuatro dígitos.
+El año mostrado debe comenzar por «31».
+El año mostrado debe cumplir las condiciones de la tarea.
+*/
+
+public class YoungFriend {
+	public static void main(String[] args) {
+	    Integer year = 3126;
+	    Integer yearBefore = 8;
+	    
+	    Integer yearActual = year - yearBefore;
+	    System.out.println(yearActual);
+	}
+}
