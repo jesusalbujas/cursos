aa97311f071d52437fbb454258448289de197dd5
jesusalbujas <jesusramirez35000@gmail.com>
2024-09-25 09:01:07 -0400

    Add applyjob task

---
+++ Operadores/src/org/cursos/CodeGym/ApplyJob.java
@@ -0,0 +1,23 @@
+package org.cursos.CodeGym;
+
+/*
+En el método main, declare variables intValue, numericValue, size, y number con tipo int.
+Asgíneles diferentes valores. Utilice una sola declaración para crear e inicializar las variables.
+Los valores pueden ser cualquier número entero.
+
+Requirements:
+•	En el método main, declare cuatro variables de tipo int.
+•	Los nombres de las variables deben ser intValue, numericValue, size y number.
+•	Las variables deben asignarse valores inmediatamente.
+•	Todas las variables deben tener valores diferentes.
+
+También puede declarar varias variables en una sola línea. En este caso, el comando se verá así:
+            int name1 = value1, name2 = value2, name3 = value3;
+ */
+
+public class ApplyJob {
+    public static void main(String[] args) {
+        int intValue = 1, numericValue = 2 , size = 3, number = 4;
+    }
+    
+}
