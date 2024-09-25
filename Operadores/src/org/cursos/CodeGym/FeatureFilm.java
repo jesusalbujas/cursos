1dfb139475f67bba529195e6e3a924464d1659d7
jesusalbujas <jesusramirez35000@gmail.com>
2024-09-25 09:10:18 -0400

    Add FeatureFilm task

---
+++ Operadores/src/org/cursos/CodeGym/FeatureFilm.java
@@ -0,0 +1,24 @@
+package org.cursos.CodeGym;
+
+
+/*
+La variable centimeters contiene una cantidad de centímetros.
+Establezca la variable meters igual al número de metros completos en centimeters (1 m = 100 cm).
+Para realizar el cálculo, use la variable centimeters y el operador de división.
+Use una sola instrucción para declarar e inicializar la variable meters.
+
+Requirements:
+•	No cambie el valor de la variable centimeters.
+•	Necesita usar la variable centimeters y el operador de división para calcular el valor de la variable meters.
+•	El programa debe mostrar el valor de la variable meters.
+
+ */
+public class FeatureFilm {
+    public static void main(String[] args) {
+        int centimeters = 243;
+
+        int meters = centimeters / 100; //escribe aquí tu código
+
+        System.out.println(meters);        
+    }
+}
