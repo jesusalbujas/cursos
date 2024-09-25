795c4fab42942c1d563c756ca97577fe17890fc7
jesusalbujas <jesusramirez35000@gmail.com>
2024-09-25 08:50:12 -0400

    Add PrintTwelveAndTwo.java

---
+++ Operadores/src/org/cursos/PrintTwelveAndTwo.java
@@ -0,0 +1,28 @@
+package org.cursos;
+
+/*
+Quita el comentario de una línea para que el programa muestre los números 12 y 2 (primero 12 y luego 2).
+
+Requirements:
+•	El programa debe mostrar los números 12 y 2.
+•	No cambies las líneas que declaran variables.
+•	El programa debe tener solo dos variables int.
+•	No cambies las líneas responsables de la salida por pantalla.
+•	Debes quitar el comentario de una línea y dejar sin modificar las demás.
+*/
+
+public class PrintTwelveAndTwo {
+	public static void main(String[] args) {
+        int x = 2;
+        int y = 12;
+
+        // y = x * y;
+        y = x + y;
+
+        x = y - x;
+        y = y - x;
+
+        System.out.println(x);
+        System.out.println(y);
+	}
+}
